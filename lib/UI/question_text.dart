7b887712dd37630f9c0a386d7b6790bb0339c002
Vedaant-rajoo <vedaant12345@gmail.com>
2018-12-31 16:32:02 +0530

    tbrr

--- lib/UI/question_text.dart
+++ lib/UI/question_text.dart
@@ -1,25 +1,44 @@
 import "package:flutter/material.dart";
 
 class QuestionText extends StatefulWidget {
-
-
   final String _question;
   final int _questionNumber;
 
-  QuestionText(this._question,this._questionNumber);
+  QuestionText(this._question, this._questionNumber);
   @override
   State createState() => new QuestionTextState();
 }
 
-class QuestionTextState extends State<QuestionText> {
+class QuestionTextState extends State<QuestionText>
+    with SingleTickerProviderStateMixin {
+  Animation<double> _fontSizeAnimation;
+  AnimationController _fontAnimationController;
+  @override
+  void initState() {
+    super.initState();
+    _fontAnimationController = new AnimationController(
+        duration: new Duration(milliseconds: 500), vsync: this);
+    _fontSizeAnimation = new CurvedAnimation(
+        parent: _fontAnimationController, curve: Curves.linear);
+        _fontSizeAnimation.addListener(()=> this.setState(() {}));
+        _fontAnimationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Material(
         color: Colors.white,
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 20.0),
-          child: new Center(child: new Text("Statement "+widget._questionNumber.toString()+": "+widget._question)),
-          )
+          child: new Center(
+              child: new Text("Statement " +
+                  widget._questionNumber.toString() +
+                  ": " +
+                  widget._question,
+                  style: new TextStyle(fontSize: _fontSizeAnimation.value*15),
+                  ),
+                  )
+        ),
         );
   }
 }
